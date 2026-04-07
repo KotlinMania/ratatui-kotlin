2e283e0038628fbe292ccb37ac692d313934e7b5
Sydney Renee <sydney@solace.ofharmony.ai>
2026-04-07 14:05:25 -0700

    Restore buffer module KDoc

--- src/commonMain/kotlin/ratatui/buffer/BufferModule.kt
+++ src/commonMain/kotlin/ratatui/buffer/BufferModule.kt
@@ -5,5 +5,21 @@ package ratatui.buffer
  * A module for the [Buffer] and [Cell] types.
  *
  * Transliteration of `ratatui-core/src/buffer.rs`.
+ *
+ * Rust source:
+ * ```rust
+ * #![warn(missing_docs)]
+ * //! A module for the [`Buffer`] and [`Cell`] types.
+ *
+ * mod assert;
+ * mod buffer;
+ * mod cell;
+ * mod cell_width;
+ * mod diff;
+ *
+ * pub use buffer::Buffer;
+ * pub use cell::{Cell, CellDiffOption};
+ * pub use cell_width::CellWidth;
+ * pub use diff::BufferDiff;
+ * ```
  */
-
