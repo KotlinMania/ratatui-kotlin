9a26ea077cec2ff699687205e71f0eedff3473d0
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-19 22:19:50 -0700

    build: use Maven coordinates for dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,31 +17,3 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "ratatui-kotlin"
-
-fun includeSiblingPort(path: String, coordinate: String) {
-    val siblingBuild = file(path)
-    if (siblingBuild.isDirectory) {
-        includeBuild(siblingBuild) {
-            dependencySubstitution {
-                substitute(module(coordinate)).using(project(":"))
-            }
-        }
-    }
-}
-
-includeSiblingPort("../anstyle-kotlin", "io.github.kotlinmania:anstyle-kotlin")
-includeSiblingPort("../bitflags-kotlin", "io.github.kotlinmania:bitflags-kotlin")
-includeSiblingPort("../itertools-kotlin", "io.github.kotlinmania:itertools-kotlin")
-includeSiblingPort("../kasuari-kotlin", "io.github.kotlinmania:kasuari-kotlin")
-includeSiblingPort("../lru-kotlin", "io.github.kotlinmania:lru-kotlin")
-includeSiblingPort("../proc-macro2-kotlin", "io.github.kotlinmania:proc-macro2-kotlin")
-includeSiblingPort("../quote-kotlin", "io.github.kotlinmania:quote-kotlin")
-includeSiblingPort("../serde-kotlin", "io.github.kotlinmania:serde-kotlin")
-includeSiblingPort("../strum-kotlin", "io.github.kotlinmania:strum-kotlin")
-includeSiblingPort("../syn-kotlin", "io.github.kotlinmania:syn-kotlin")
-includeSiblingPort("../thiserror-kotlin", "io.github.kotlinmania:thiserror-kotlin")
-includeSiblingPort("../time-kotlin", "io.github.kotlinmania:time-kotlin")
-includeSiblingPort("../unicode-segmentation-kotlin", "io.github.kotlinmania:unicode-segmentation-kotlin")
-includeSiblingPort("../unicode-width-kotlin", "io.github.kotlinmania:unicode-width-kotlin")
-includeSiblingPort("../anyhow-kotlin", "io.github.kotlinmania:anyhow-kotlin")
-includeSiblingPort("../crossterm-kotlin", "io.github.kotlinmania:crossterm-kotlin")
