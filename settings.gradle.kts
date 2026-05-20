96419010723a2d4b5fb88369d6eb86f88d2676d4
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-19 19:37:56 -0700

    build: resolve ratatui siblings from local ports

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,3 +17,27 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "ratatui-kotlin"
+
+fun includeSiblingPort(path: String, coordinate: String) {
+    val siblingBuild = file(path)
+    if (siblingBuild.isDirectory) {
+        includeBuild(siblingBuild) {
+            dependencySubstitution {
+                substitute(module(coordinate)).using(project(":"))
+            }
+        }
+    }
+}
+
+includeSiblingPort("../anstyle-kotlin", "io.github.kotlinmania:anstyle-kotlin")
+includeSiblingPort("../bitflags-kotlin", "io.github.kotlinmania:bitflags-kotlin")
+includeSiblingPort("../itertools-kotlin", "io.github.kotlinmania:itertools-kotlin")
+includeSiblingPort("../kasuari-kotlin", "io.github.kotlinmania:kasuari-kotlin")
+includeSiblingPort("../lru-kotlin", "io.github.kotlinmania:lru-kotlin")
+includeSiblingPort("../serde-kotlin", "io.github.kotlinmania:serde-kotlin")
+includeSiblingPort("../strum-kotlin", "io.github.kotlinmania:strum-kotlin")
+includeSiblingPort("../thiserror-kotlin", "io.github.kotlinmania:thiserror-kotlin")
+includeSiblingPort("../time-kotlin", "io.github.kotlinmania:time-kotlin")
+includeSiblingPort("../unicode-segmentation-kotlin", "io.github.kotlinmania:unicode-segmentation-kotlin")
+includeSiblingPort("../unicode-width-kotlin", "io.github.kotlinmania:unicode-width-kotlin")
+includeSiblingPort("../crossterm-kotlin", "io.github.kotlinmania:crossterm-kotlin")
