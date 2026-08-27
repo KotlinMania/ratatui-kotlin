377e6739e9fdcc351785dd4d929bc2e9dd2ee7fd
Sydney Renee <sydney@solace.ofharmony.ai>
2026-08-26 17:17:50 -0700

    build(deps): align Kotlin and AGP versions with template

--- swift-test-harness/Package.swift
+++ swift-test-harness/Package.swift
@@ -1,23 +1,34 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "SwiftTestHarness",
-    platforms: [.macOS(.v14)],
+    platforms: [
+        .macOS(.v14),
+    ],
     dependencies: [
         .package(name: "Ratatui", path: "../build/SPMPackage/macosArm64/Debug")
     ],
     targets: [
-        .executableTarget(
+        .testTarget(
             name: "SwiftTestHarnessTests",
             dependencies: [
                 .product(name: "RatatuiLibrary", package: "Ratatui")
             ],
-            path: "Tests/SwiftTestHarnessTests",
+            swiftSettings: [
+                .unsafeFlags([
+                    "-F", "/Library/Developer/CommandLineTools/Library/Developer/Frameworks",
+                ]),
+            ],
             linkerSettings: [
                 .unsafeFlags([
                     "-L", "../build/swift-test",
                     "-lRatatui",
+                    "-F", "/Library/Developer/CommandLineTools/Library/Developer/Frameworks",
+                    "-Xlinker", "-rpath",
+                    "-Xlinker", "/Library/Developer/CommandLineTools/Library/Developer/Frameworks",
+                    "-Xlinker", "-rpath",
+                    "-Xlinker", "/Library/Developer/CommandLineTools/Library/Developer/usr/lib",
                 ]),
             ]
         ),
